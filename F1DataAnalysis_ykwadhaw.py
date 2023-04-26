351103585b00fab88806a503b07ad16d930c5d0a
typ1cal <typ1cal>
2023-04-26 01:54:17 +0000

    Final Changes to the notebook.
    Added some graphs, and provided Markdown for understanding

--- F1DataAnalysis_ykwadhaw.py
+++ F1DataAnalysis_ykwadhaw.py
@@ -47,6 +47,12 @@ for file in files:
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC 
+# MAGIC # Data Manipulation
+
+# COMMAND ----------
+
 # Loading the csv files into spark dataframe
 
 from pyspark.sql import SparkSession
@@ -90,15 +96,20 @@ display(races_details_df.head())
 
 # COMMAND ----------
 
-display(driver_details_df.describe())
-display(fastest_laps_df.describe())
-display(races_details_df.describe())
+# MAGIC %md
+# MAGIC # N/A Values
+# MAGIC One of the important aspects of data piplining is to identifying null values. This might result in bad data interpretation and might result to a wrong judgement. So it is important to identifying null values before working with data. Fortunately, we do not have any null values in our data.
 
 # COMMAND ----------
 
 display(driver_details_df.isnull().sum())
 display(fastest_laps_df.isnull().sum())
 display(races_details_df.isnull().sum())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Top 20 Manufacturers by Points Ever
 
 # COMMAND ----------
 
@@ -117,11 +128,17 @@ plt.xlabel('Constructor', fontsize=14)
 plt.ylabel('Total Points', fontsize=14)
 plt.xticks(rotation=45, fontsize=12)
 plt.yticks(fontsize=12)
-plt.title('Top 20 Constructors by Total Points', fontsize=16)
+plt.title('Top 20 Manufacturers by Total Points', fontsize=16)
 plt.tight_layout()
 plt.grid()
 plt.show()
 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC # Driver's Standing with Total Points Ever
 
 # COMMAND ----------
 
@@ -137,7 +154,11 @@ print(top_drivers_df)
 
 # COMMAND ----------
 
-
+# MAGIC %md
+# MAGIC 
+# MAGIC # Analyze the distribution of lap times and how they have changed over the years
+# MAGIC 
+# MAGIC We'll try to identify lap times for every year separtely and then try to genenrate a graph for average lap times for every year.
 
 # COMMAND ----------
 
@@ -161,6 +182,14 @@ plt.show()
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC 
+# MAGIC # Top Drivers by Points
+# MAGIC 
+# MAGIC We now try to develop a line graph for points to year graph for drivers. The longest line is of Sir Lewis Hamilton from 2014 to 2020 which shows a dominant 6 years for a F1 drivers, followed by Sebastian Vettel, and Fernando Alonso
+
+# COMMAND ----------
+
 df_driver_points = driver_details_df.groupby('Driver')['PTS'].sum().reset_index()
 df_driver_points = df_driver_points.sort_values('PTS', ascending=False)
 top_drivers = df_driver_points.head(10)
@@ -170,6 +199,14 @@ fig = px.line(top_drivers_laps, x='Year', y='PTS', color='Driver', title='Top Dr
 fig.show()
 
 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC # Barplots for Number of Grand Prix Races per Year
+# MAGIC 
+# MAGIC The graph shows a visual represents of the total number of grand prix races took place from 1950 to 2022. The number of races seem to increase but there is a little downfall in 2020, which can be because of Covid-19. A better idea can be seen when we hover the mouse around a bar plot down.
 
 # COMMAND ----------
 
@@ -184,6 +221,14 @@ fig.show()
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC 
+# MAGIC # Most Successful Drivers and Teams in Terms of Race Wins
+# MAGIC 
+# MAGIC Displaying a beautiful graph that develops a scatter plot to identify best teams with their best drivers. The information in the visual graph is represented in a better sense when we hover the mouse on the scatter. The right most dot shows Lewis Hamilton with Mercedes has 82 wins as of 2022.
+
+# COMMAND ----------
+
 df_winners = races_details_df.groupby(['Driver', 'Car']).size().reset_index(name='Wins')
 df_winners = df_winners.sort_values(by=['Wins'], ascending=False)
 fig = px.scatter(df_winners, x="Wins", y="Driver", color="Car", size="Wins", hover_name="Car",
@@ -191,4 +236,35 @@ fig = px.scatter(df_winners, x="Wins", y="Driver", color="Car", size="Wins", hov
                  title="Most Successful Drivers and Teams in Terms of Race Wins",
                  labels={"Wins": "Race Wins", "Winner": "Driver"})
 fig.update_layout(xaxis_title="Race Wins", yaxis_title="Driver", legend_title="Team")
+fig.show()
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC # Number of Wins of a Driver with their Manufacturer
+# MAGIC 
+# MAGIC This graph provides an interesting bar graph of drivers with their total wins with the Manufacturer.
+
+# COMMAND ----------
+
+df_wins_per_car = races_details_df.groupby(['Car', 'Driver'])['Driver'].count().reset_index(name='Wins')
+df_wins_per_car = df_wins_per_car.sort_values(by='Wins', ascending=False)
+fig = px.bar(df_wins_per_car, x='Car', y='Wins', color='Driver')
+fig.update_layout(title='Number of Wins per Car', xaxis_title='Car', yaxis_title='Number of Wins')
+fig.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC # Driver's Year Wise Performance
+# MAGIC 
+# MAGIC This graph provides a interesting data of driver's year wise performance. Here Max almost has straight upward line from 2020 to 2022. Which explains the performance of Redbull Team.
+
+# COMMAND ----------
+
+df_driver = driver_details_df
+df_year_driver_pts = driver_details_df.groupby(['Year', 'Driver']).sum()['PTS'].reset_index()
+fig = px.line(df_year_driver_pts, x='Year', y='PTS', color='Driver', title='Year-wise Driver Performance')
+fig.update_layout(xaxis_title='Year', yaxis_title='Points')
 fig.show()
